a409635c5641601e4a183f468764e33defe754fa
Abdullahi Busari O <84146553+abdullahi-prog@users.noreply.github.com>
2021-08-20 04:48:38 -0700

    change

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,7 +136,6 @@ STATICFILES_DIRS =[
 ]
 STATIC_ROOT =   os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 
 # Default primary key field type
